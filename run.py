ca4c609ae1979cb7f8196de11ae30878cfe7394e
Simon Moynihan <simon.moynihan33@gmail.com>
2024-07-22 10:36:00 +0000

    Install pandas, plotly and import sqlite3 and datetime

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sqlite3
+from datetime import datetime
+import plotly.express as px
+import pandas as pd 
