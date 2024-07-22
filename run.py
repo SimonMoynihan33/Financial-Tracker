c941a14dc1c93fb3b4d7ecc9d03939917dbc62e6
Simon Moynihan <simon.moynihan33@gmail.com>
2024-07-22 11:02:41 +0000

    Connect to SQL database and add logic to create a table if it does not exist

--- run.py
+++ run.py
@@ -2,3 +2,18 @@ import sqlite3
 from datetime import datetime
 import plotly.express as px
 import pandas as pd 
+
+# Connect to SQL database
+conn = sqlite3.connect('expense_tracker.db')
+c = conn.cursor()
+
+# Create table if it does not exist
+c.execute('''
+    CREATE TABLE IF DOESNT EXIST users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        user_id INTEGER NOT NULL UNIQUE
+    )
+''')
+conn.commit()
+
