1224216c825c2d40f7185a026f13d196626b45b5
Simon Moynihan <simon.moynihan33@gmail.com>
2024-07-23 16:01:08 +0000

    Remove date prompt as I could not get to work

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ current_user_id = None
 conn = sqlite3.connect('expense_tracker.db')
 c = conn.cursor()
 
-# Create table if it does not exist
+# Create table 
 c.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,7 +24,6 @@ c.execute('''
     user_id INTEGER NOT NULL,
     amount REAL NOT NULL,
     category TEXT NOT NULL,
-    date TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (user_id)
     )
 ''')
@@ -46,7 +45,7 @@ def greet_msg():
             break
         elif response == 'n':
             print('You must register to use this app')
-            register_user() # Call registration function
+            register_user() 
             break
         else:
             print('Invalid input. Please enter "y" for yes or "n" for no.')
@@ -60,7 +59,7 @@ def register_user():
     while True:
         name = input('Enter your name: ').capitalize()
         if name.upper() == 'BACK':
-            greet_msg() # Return to start function
+            greet_msg()
             continue        
         # Check if the name is aplhabetical
         elif not name.isalpha():
@@ -88,7 +87,7 @@ def register_user():
             except Exception as e:
                 print(f'An error occurred: {e}')
         elif user_id == 'BACK':
-            greet_msg() # Return to start function
+            greet_msg()
             continue
         else:
             print('Invalid ID. It must be 4 digits long.')
@@ -103,12 +102,12 @@ def user_login():
     while True:
         user_input_id = input('Please enter your unique 4 digit code: ').strip()
         if user_input_id.upper() == 'BACK':
-            greet_msg() # Return to start function
+            greet_msg()
             continue
 
         # Validate and process the user ID
         if len(user_input_id) == 4 and user_input_id.isdigit():
-            user_input_id = int(user_input_id) # Convert to integer
+            user_input_id = int(user_input_id)
             valid, name = check_user_id(user_input_id) # Check user ID and recieve both status and name
             if valid:
                 current_user_id = user_input_id
@@ -128,9 +127,9 @@ def check_user_id(user_id):
     c.execute('SELECT name FROM users WHERE user_id = ?', (user_id,))
     result = c.fetchone()
     if result is None:
-        return False, None # Return false and none for id and name 
+        return False, None
     else:
-        name = result[0] # Extract name from query result
+        name = result[0] 
         return True, name
 
 
@@ -154,25 +153,14 @@ def add_expense():
     """
     Function to add an expense
     """
-    global current_user_id
     amount = float(input('Enter the expense amount: '))
     category = input('Enter the category of the expense: ').capitalize()
-    date = input('Enter the date of expenses (DD-MM-YYYY) or leave blank for today: ')
-    if not date:
-        date = datetime.today().strftime('%d-%m-%Y')
-    else:
-        # Validate date format
-        try:
-            datetime.strptime(date, '%d-%m-%Y')
-        except ValueError:
-            print('Incorrect date format, should be DD-MM-YYYY')
-            return add_expense()
 
     # Insert expenses into database
-    c.execute('INSERT INTO expenses (user_id, amount, category, date) VALUES (?, ?, ?, ?)', (current_user_id, amount, category, date))
+    c.execute('INSERT INTO expenses (amount, category) VALUES (?, ?, ?)', (current_user_id, amount, category))
     conn.commit()
 
-    print(f'Expense of {amount} in category {category} on {date} added.')
+    print(f'Expense of {amount} in category {category} added.')
 
 
 def get_report():
